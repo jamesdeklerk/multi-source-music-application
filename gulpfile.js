4b6383cb964ec3bf94ff0f3bcaaad533e49c768e
jamesdeklerk <jamesdeklerk@gmail.com>
2016-08-27 17:11:35 +0200

    Setup config files for TypeScript, Node.js, Gulp and TSLint.

---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+// =======================================================
+// Required files & Directories
+// =======================================================
+var gulp = require('gulp'),
+    typescript = require('gulp-typescript'),
+    browsersync = require('browser-sync'),
+    reload = browsersync.reload,
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    // Using tsconfig.json
+    typescriptProject = typescript.createProject('tsconfig.json'),
+    // Directories
+    sourceDirectory = 'src/',
+    buildDirectory = 'build/',
+    htmlDirectory = sourceDirectory,
+    cssDirectory = buildDirectory + 'css/',
+    javascriptDirectory = buildDirectory + 'javascript/',
+    scssDirectory = sourceDirectory + 'scss/',
+    typescriptDirectory = sourceDirectory + 'typescript/';
+// -------------------------------------------------------
+
+
+
+// =======================================================
+// Recompile tasks
+// =======================================================
+
+// Compile SCSS to CSS
+gulp.task('recompile:scss', function () {
+    return gulp.src(scssDirectory + '**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest(cssDirectory));
+});
+
+// Compile TypeScript to JavaScript - using tsconfig.json
+gulp.task('recompile:typescript', function () {
+    return typescriptProject.src()
+        .pipe(typescript(typescriptProject))
+        .pipe(gulp.dest(javascriptDirectory));
+});
+
+// Recompile watch task - watches for files to recompile
+gulp.task('recompile:watch', function () {
+    gulp.watch(scssDirectory + '**/*.scss', ['recompile:scss']);
+    gulp.watch(typescriptDirectory + '**/*.ts', ['recompile:typescript']);
+});
+
+// Recompile task - recompiles all files
+gulp.task('recompile', ['recompile:scss', 'recompile:typescript']);
+
+// -------------------------------------------------------
+
+
+
+// =======================================================
+// Reload tasks - for Browsersync
+// =======================================================
+
+// Browsersync Task
+gulp.task('browsersync', function () {
+    browsersync({
+        server: {
+            baseDir: "./" + sourceDirectory // setting the base directory
+        }
+    });
+});
+
+// Reload HTML
+gulp.task('reload:html', function () {
+    gulp.src(htmlDirectory + '**/*.html')
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+// Reload CSS
+gulp.task('reload:css', function () {
+    gulp.src(cssDirectory + '**/*.css')
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+// Reload JavaScript
+gulp.task('reload:javascript', function () {
+    gulp.src(javascriptDirectory + '**/*.js')
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+// Reload watch task - watches for files to reload
+gulp.task('reload:watch', function () {
+    gulp.watch(htmlDirectory + '**/*.html', ['reload:html']);
+    gulp.watch(cssDirectory + '**/*.css', ['reload:css']);
+    gulp.watch(javascriptDirectory + '**/*.javascript', ['reload:javascript']);
+});
+
+// Reload task - starts Browsersync & reloads all files
+gulp.task('reload', ['browsersync', 'reload:html', 'reload:css', 'reload:javascript']);
+
+// -------------------------------------------------------
+
+
+
+// =======================================================
+// Default task - starts all others
+// =======================================================
+gulp.task('default', ['recompile', 'recompile:watch', 'reload', 'reload:watch']);
+// -------------------------------------------------------
