26044090314d4f7f62cba78cc4dd607ba2035d25
jamesdeklerk <jamesdeklerk@gmail.com>
2016-08-28 08:20:23 +0200

    Playing around with Angular $routeProvider.

---
+++ src/typescript/ui/ui-scripts.ts
@@ -0,0 +1,50 @@
+angular.module('music-app', ['ngRoute', 'firebase'])
+ 
+.value('fbURL', 'https://ng-projects-list.firebaseio.com/')
+.service('fbRef', function(fbURL) {
+  return new Firebase(fbURL)
+})
+ 
+.config(function($routeProvider) {
+  var resolveProjects = {
+    projects: function () {
+      return "";
+    }
+  };
+ 
+  $routeProvider
+    .when('/', {
+      controller:'ProjectListController as projectList',
+      templateUrl:'src/html/pages/list.html',
+      resolve: resolveProjects
+    })
+    .when('/edit/:projectId/', {
+      controller:'EditProjectController as editProject',
+      templateUrl:'src/html/pages/detail.html',
+      resolve: resolveProjects
+    })
+    .when('/new', {
+      controller:'NewProjectController as editProject',
+      templateUrl:'src/html/pages/detail.html',
+      resolve: resolveProjects
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+ 
+.controller('ProjectListController', function(projects) {
+  this.controller = "ProjectListController";
+})
+ 
+.controller('NewProjectController', function(projects) {
+  this.controller = "NewProjectController";
+})
+ 
+.controller('EditProjectController', function(projects, $routeParams) {
+  this.controller = "EditProjectController";
+  this.projectId = $routeParams.projectId;
+  this.search = $routeParams.search;
+  this.other = $routeParams.other;
+});
+
