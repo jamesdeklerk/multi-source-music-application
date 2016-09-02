e3fac90c2b58fa0beb44de1474d2aa966d55424e
jamesdeklerk <jamesdeklerk@gmail.com>
2016-09-02 21:33:51 +0200

    Implemented the majority of the YouTubeAdapter's functionality.

--- src/typescript/classes/player_adapters/YouTubeAdapter.ts
+++ src/typescript/classes/player_adapters/YouTubeAdapter.ts
@@ -1,46 +1,86 @@
+interface Window {
+    onYouTubeIframeAPIReady: any;
+}
+
 class YouTubeAdapter implements IPlayerAdapter {
 
-    constructor() {
-
-    }
+    private youtubePlayer: YT.Player;
 
     public initialize(): void {
-        throw `Not Implemented`;
+
+        // Load the IFrame Player API code asynchronously.
+        let tag = <HTMLScriptElement> document.createElement(`script`);
+        tag.src = "https://www.youtube.com/iframe_api";
+
+        let firstScriptTag = document.getElementsByTagName(`script`)[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        function onPlayerReady() {
+            // window.youtubePlayer = this.youtubePlayer;
+            console.log("Hey! I'm ready! :)");
+        }
+
+        // Create an <iframe> (and YouTube player) after the API code downloads.
+        // tslint:disable-next-line
+        let _this = this;
+        window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
+
+            // May not be in the right scope, probably have to use _this.
+            console.log(this);
+
+            _this.youtubePlayer = new YT.Player(`youtube-player`, {
+                events: {
+                    "onReady": onPlayerReady,
+                },
+                height: `300`,
+                playerVars: {
+                    "controls": 0,
+                },
+                videoId: ``, // CTB0rm_Figk
+                width: `600`,
+            });
+        };
+
     }
 
     public load(track: ITrack): void {
+
+        // Get YouTube ID from track then load it.
+        // this.youtubePlayer.loadVideoById(...);
+
         throw `Not Implemented`;
     }
 
     public play(): void {
-        throw `Not Implemented`;
+        this.youtubePlayer.playVideo();
     }
 
     public pause(): void {
-        throw `Not Implemented`;
+        this.youtubePlayer.pauseVideo();
     }
 
     public stop(): void {
-        throw `Not Implemented`;
+        this.youtubePlayer.stopVideo();
     }
 
     public setVolume(volume: number): void {
-        throw `Not Implemented`;
+        this.youtubePlayer.setVolume(volume * 100);
     }
 
     public seekTo(milliseconds: number): void {
-        throw `Not Implemented`;
+        this.youtubePlayer.seekTo(milliseconds / 1000, true);
     }
 
     public getCurrentTime(): number {
-        throw `Not Implemented`;
+        return this.youtubePlayer.getCurrentTime() * 1000;
     }
 
     public getDuration(): number {
-        throw `Not Implemented`;
+        return this.youtubePlayer.getDuration() * 1000;
     }
 
     public getPercentageLoaded(): number {
-        throw `Not Implemented`;
+        return this.youtubePlayer.getVideoLoadedFraction();
     }
+
 }
